a4889668703a793b7e28b845008ed1fd6d8550dd
Dan Kreibich <dan.a.kreibich@gmail.com>
2023-01-22 11:23:53 +0100

    Added view, routes and controller to extract data from CSV and add to DB

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #homepage
   root to: "pages#home"
 
+  # Everythin related to "Question Trainer"
   get "/overview", to: "questions#overview", as: "overview"
+
+  # Routes for everything related to uploading CSV data
+  get "csv/new", to: "csv#new"
+  post 'csv/new', to: 'csv#create'
+  get 'csv/upload_success_page', to: 'csv#upload_success_page'
 end
